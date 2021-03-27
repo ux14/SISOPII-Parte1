616fa6c317f62fdebd00835246f86e756b3c3143
Júlia D. Craide <juliadcraide@hotmail.com>
2021-03-27 16:17:18 -0300

    added files.h

---
+++ server/files.h
@@ -0,0 +1,14 @@
+#ifndef FILES_H
+#define FILES_H
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void createUser(string username);
+void follow(string follower, string followed);
+vector<string> getUsers();
+vector<string> getFollowers(string username);
+
+#endif
