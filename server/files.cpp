0d517a5783ae8cc93056ee1acb3131aacc612b35
Júlia D. Craide <juliadcraide@hotmail.com>
2021-03-27 16:17:56 -0300

    fixing files path

--- server/files.cpp
+++ server/files.cpp
@@ -1,24 +1,25 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-void createUsuer(string username){
+void createUser(string username)
+{
     FILE *users_arq, *followers_arq;
 
     //Add user to users file
-    users_arq = fopen("files/users.txt", "at");
+    users_arq = fopen("server/files/users.txt", "at");
     fprintf(users_arq, (username + '\n').c_str());
     fclose(users_arq);
 
-    //Creates follower file
-    string followersArqDir = "files/followers/" + username + ".txt";
+    // //Creates follower file
+    string followersArqDir = "server/files/followers/" + username + ".txt";
     followers_arq = fopen(followersArqDir.c_str(), "wt");
     fclose(followers_arq);
 }
 
 void follow(string follower, string followed)
 {
-    string followersArqDir = "files/followers/" + followed + ".txt";
+    string followersArqDir = "server/files/followers/" + followed + ".txt";
 
     FILE *arq;
     arq = fopen(followersArqDir.c_str(), "at");
@@ -31,7 +32,7 @@ vector<string> getUsers()
     vector<string> users;
     char user[30];
     FILE *arq;
-    arq = fopen("files/users.txt", "rt");
+    arq = fopen("server/files/users.txt", "rt");
     while (fgets(user, 30, arq) != NULL)
     {
         //remove \n
@@ -46,7 +47,7 @@ vector<string> getFollowers(string username)
 {
     vector<string> followers;
     char follower[30];
-    string followersArqDir = "files/followers/" + username + ".txt";
+    string followersArqDir = "server/files/followers/" + username + ".txt";
 
     FILE *arq;
     arq = fopen(followersArqDir.c_str(), "rt");
@@ -58,33 +59,4 @@ vector<string> getFollowers(string username)
     }
     fclose(arq);
     return followers;
-}
-
-int main()
-{
-
-    createUsuer("User1");
-    createUsuer("User2");
-    createUsuer("User3");
-
-    follow("User2", "User1");
-    follow("User3", "User1");
-    follow("User3", "User2");
-
-    vector<string> users = getUsers();
-    vector<string> followers = getFollowers("User1");
-
-    cout << "All users:" << endl;
-    for (int i = 0; i < users.size(); i++)
-    {
-        cout << users[i] << endl;
-    }
-
-    cout << "User 1 followers:" << endl;
-    for (int i = 0; i < followers.size(); i++)
-    {
-        cout << followers[i] << endl;
-    }
-
-    return 0;
 }
